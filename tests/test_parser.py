2a31562c9b701fa8b04ab0eddc04ce5f9bb04058
The-Canuck <morrisonman56@gmail.com>
2019-07-20 22:09:55 -0600

    Update test case

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,13 +5,13 @@ from pgn_parser import PGNParser
 
 class TestParser(TestCase):
     def test_parse_pgn(self):
-        parser = PGNParser.Parser('tests/test.pgn')
+        parser = PGNParser.Parser('test.pgn')
         games = parser.parse_pgn()
 
         game_string = ''
         for game in games:
             game_string = game_string + game.__str__() + '\n'
 
-        with open('tests/correct_out.txt') as file:
+        with open('correct_out.txt') as file:
             correct = file.read()
         self.assertTrue(game_string == correct)
